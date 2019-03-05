00c69bd08207f335fec15f77f6bccd68dae559c2
Eugene <mail@apoheliy.com>
2019-03-06 01:31:41 +0300

    Improve selection of predicted step

--- solvers/tetragonal_neural/miner_dnn.py
+++ solvers/tetragonal_neural/miner_dnn.py
@@ -7,7 +7,8 @@ import copy
 class TensorFlowSweeper:
     def __init__(self) -> None:
         self.__margin_size_ = 4
-        self.__no_probability_ = -100.0
+        self.__minimal_clear_probability_ = 0.6
+        self.__max_probability_ = 1.0
         self.__signs_ = [' ', '.', '*', '0', '1', '2', '3', '4', '5', '6', '7', '8']
         self.__solver_ = tf.estimator.DNNClassifier(feature_columns = self.__CreateColumns(), hidden_units=[960, 140, 21], n_classes=3, model_dir="data/minesweeper-bot")
 
@@ -21,6 +22,8 @@ class TensorFlowSweeper:
         pass
 
     def __GetColumnName(self, row, col):
+        if row == 0 and col == 0:
+            raise ValueError("Logic error: request name for cel (0, 0)")
         return "C" + str(row) + "." + str(col)
 
 
@@ -79,35 +82,32 @@ class TensorFlowSweeper:
         positions = self.__GetPredictPositions(field)
         if len(positions) == 0:
             raise ValueError("Empty list of cells for prediction")
-        predict = self.__solver_.predict(input_fn = lambda: self.__PredictInputData(field, positions))
+        predict = self.__solver_.predict(input_fn = lambda: self.__PredictInputData(field, positions), yield_single_examples = False)
         clear_cell = []
-        clear_probability = self.__no_probability_
-        unknown_cell = []
-        unknown_probability = self.__no_probability_
-        row = 0
-        for forecast in predict:
-            if row >= len(positions):
-                break
-            forecast_value = forecast['class_ids'][0]
-            clear_probe = forecast["probabilities"][0]
-            unknown_probe = forecast["probabilities"][2]
-            if forecast_value == 0 and clear_probe > clear_probability:
-                # predict first clear cell
-                clear_cell = positions[row]
+        clear_probability = self.__minimal_clear_probability_
+        non_mine_cell = []
+        mine_probability = self.__max_probability_
+        forecast = next(predict)
+        for index in range(len(positions)):
+            clear_probe = forecast["probabilities"][index][0]
+            mine_probe = forecast["probabilities"][index][1]
+            if clear_probe > clear_probability:
+                # predict clear cell. Select max probability
+                clear_cell = positions[index]
                 clear_probability = clear_probe
-            # if forecast_value == 1:
-                # predict mine. Nothing doing
-                # field[positions[row][0]][positions[row][1]] = '*'
-            if forecast_value == 2 and unknown_probe > unknown_probability:
-                # predict unknown
-                unknown_cell = positions[row]
-                unknown_probability = unknown_probe
-            row += 1
-        if len(clear_cell) == 0:
-            if len(unknown_cell) == 0:
-                return positions[0]
-            return unknown_cell
-        return clear_cell
+            if mine_probe < mine_probability:
+                # predict unknown. Select minimal mine probability
+                non_mine_cell = positions[index]
+                mine_probability = mine_probe
+        if len(clear_cell) != 0:
+            # debug
+            print("Clear probability: ", clear_probability, sep='')
+            return clear_cell
+        if len(non_mine_cell) != 0:
+            # debug
+            print("Mine probability: ", mine_probability, sep='')
+            return non_mine_cell
+        raise ValueError("can't find cell with max clear-state or min mine-state")
 
 
     def __AddDnnData(self, field, target_row, target_col, features):
