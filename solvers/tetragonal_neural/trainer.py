59d4ac712bea25b24ebbe7acac34d48b2677fbcf
Кислов Евгений <dev@evgenykislov.com>
2019-04-23 09:17:34 +0300

    Add training of unknown data. Don't train the full covered field

--- solvers/tetragonal_neural/trainer.py
+++ solvers/tetragonal_neural/trainer.py
@@ -12,12 +12,24 @@ class Step:
         self.__ReadStep(folder, filename)
 
 
+    def FullClosedField(self):
+        for row in self.__field_:
+            for col in row:
+                if col != '.' and col != ' ':
+                    return False
+        return True
+
+
     def GetField(self):
         return self.__field_
 
 
     def GetTraining(self):
-        return self.__step_row_, self.__step_col_, self.__step_result_
+        list = []
+        list.append([self.__step_row_, self.__step_col_, self.__step_result_])
+        if random.randint(0, 9) == 0:
+            list.append([self.__step_row_, self.__step_col_, '?'])
+        return list
 
 
     def RemoveStep(self):
@@ -70,6 +82,7 @@ class Step:
 
 
 # main() - Base logic of gaming
+random.seed()
 sweeper = miner_dnn.TensorFlowSweeper()
 data_folder = "train"
 print("Tetragonal model trainer. Evgeny Kislov, 2019")
@@ -78,8 +91,11 @@ while (True):
     files = [f for f in os.listdir(data_folder) if (len(f) >= 9 and f[0:5] == "step_" and f[-4:] == ".txt")]
     for f in files:
         step = Step(data_folder, f)
-        sweeper.Train(step.GetField(), [step.GetTraining()])
-        print("Field ", f, " processed", sep='')
+        if step.FullClosedField():
+            print("Field ", f, " is full closed. Pass it", sep='')
+        else:
+            sweeper.Train(step.GetField(), step.GetTraining())
+            print("Field ", f, " processed", sep='')
         step.RemoveStep()
     # There aren't files in list
     time.sleep(5)
